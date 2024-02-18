e28942ac9c39bad840f151dcf72db037c939c324
Silviu Stroe <silviu@silviu-s.com>
2024-02-18 20:32:07 +0200

    Add missing includes

--- src/svxlink/reflector/ReflectorClient.h
+++ src/svxlink/reflector/ReflectorClient.h
@@ -58,6 +58,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include "ReflectorMsg.h"
 #include "ProtoVer.h"
+#include <mutex>
+#include <vector>
 
 
 /****************************************************************************
